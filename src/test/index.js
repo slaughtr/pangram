e2e0b110b46a3b2574a1f6d494eb5aa73d544be4
Dallas Slaughter <dslaughtr@gmail.com>
2020-04-18 13:49:54 -0700

    [bugfix] - Add async/await to tests to reflect change in handler

--- src/test/index.js
+++ src/test/index.js
@@ -5,24 +5,29 @@ const { handler } = require('../index');
 const { pangrams, notPangrams } = require('./pangrams');
 
 describe('handler', () => {
-    it('Should return true for all items in pangrams array', () => {
+    it('Should return true for all items in pangrams array', async () => {
         for (const string of pangrams) {
-            const { body } = handler({queryStringParameters: {string}});
+            const { body } = await handler({queryStringParameters: {string}});
             expect(JSON.parse(body)).to.have.property('isPangram').to.equal(true);
         }
     })
 
-    it('Should return false for all items in notPangrams array', () => {
+    it('Should return false for all items in notPangrams array', async () => {
         for (const string of notPangrams) {
-            const { body } = handler({queryStringParameters: {string}});
+            const { body } = await handler({queryStringParameters: {string}});
             expect(JSON.parse(body)).to.have.property('isPangram').to.equal(false);
         }
     })
 
-    it('Should return a 200 status code for all items in both arrays', () => {
+    it('Should return a 200 status code for all items in both arrays', async () => {
         for (const string of [...pangrams, ...notPangrams]) {
-            const { statusCode } = handler({queryStringParameters: {string}});
+            const { statusCode } = await handler({queryStringParameters: {string}});
             expect(statusCode).to.equal(200);
         }
+    })
+
+    it('Should return 503 on a naked call', async () => {
+            const { statusCode } = await handler();
+            expect(statusCode).to.equal(503);
     })
 })
